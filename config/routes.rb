6097d22bfa6d348ca491fcbd6ae5aca6f3790e84
ange.saavedra17@gmail.com <ange.saavedra17@gmail.com>
2024-06-14 16:58:36 +0200

    in progress

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :bookings do
-    resources :cars
+  resources :cars do
+    resources :bookings
   end
   get 'my_cars', to: 'cars#my_cars'
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
+require 'faker'
+
+Car.destroy_all
+
+puts 'Creating 5 cars...'
+
+5.times do
+  Car.create!(
+    name: Faker::Vehicle.make_and_model,
+    photo: "https://source.unsplash.com/random/800x600?car",
+    address: Faker::Address.full_address,
+    price: rand(9000..900000),
+    user_id: User.first.id
+  )
+end
+
+puts '5 cars created!'
+
+
+
+
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
