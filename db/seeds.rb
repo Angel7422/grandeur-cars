04ae107f1f39fbab9096d88b0c2bdcefd431c681
aureliendieffenbach <aurelien.dieffenbach@gmail.com>
2024-06-14 19:06:16 +0200

    cars-index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Car.destroy_all
+
+puts 'Creating 5 cars...'
+
+5.times do
+  Car.create!(
+    name: Faker::Vehicle.make_and_model,
+    photo: "https://loremflickr.com/320/240/supercar",
+    address: Faker::Address.full_address,
+    price: rand(1000..3000),
+    user_id: User.first.id
+  )
+end
+
+puts '5 cars created!'
