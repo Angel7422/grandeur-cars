069842c1ea487bfff8e3e58995781e0ee4e6a0df
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-06-16 15:58:07 +0200

    changement dans create du controller-booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_13_190236) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_134428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_190236) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_price"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
