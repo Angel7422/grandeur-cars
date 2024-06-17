d1e77d2d80fbab729903612b6cb07f0443342d2a
ange.saavedra17@gmail.com <ange.saavedra17@gmail.com>
2024-06-17 15:58:26 +0200

    modification home action in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @cars = Car.all
+    @bookings = Booking.all
   end
 
   def dashboard
