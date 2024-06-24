758fb6469bf544e6d386662670b2039304e1d61e
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-06-24 15:27:51 +0200

    button booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   resources :bookings, only: [:index]
+
   resources :cars do
     resources :bookings, except: [:index]
   end
-  resources :bookings, only: [:index]
+
   get 'my_cars', to: 'cars#my_cars'
   get 'dashboard', to: 'pages#dashboard'
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
