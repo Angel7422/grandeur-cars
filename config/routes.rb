2525d23c12f65891c020190d595ea0c27c0a95bb
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-06-25 17:49:50 +0200

    problème avec les merges

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :bookings, only: [:index]
-<<<<<<< HEAD
-
-=======
-    # member do
-    #   patch 'validate', to: 'bookings#validate', as: :validate
-    #   patch 'reject', to: 'bookings#reject', as: :reject
-    # end
->>>>>>> 2d45d21de47de669dc3bee1fdb70f34e98110203
   resources :cars do
     resources :bookings, except: [:index]
   end
